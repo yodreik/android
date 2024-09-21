0deac07e982b2c500a2fe873d3385af9430099fe
Artem F. <artfa63@gmail.com>
2024-09-21 13:49:25 +0400

    Initial commit

---
+++ app/src/main/java/com/example/yodreik/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.yodreik;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.yodreik.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void loginButtonOnClick(View view) {
+        Toast.makeText(getApplicationContext(), "Not Implemented", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/yodreik/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.yodreik;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.yodreik.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void loginButtonOnClick(View view) {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    public void registerButtonOnClick(View view) {
+        Toast.makeText(getApplicationContext(), "Not Implemented", Toast.LENGTH_SHORT).show();
+    }
+}
