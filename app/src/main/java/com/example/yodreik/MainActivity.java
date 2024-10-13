e69a784857369e9e8057cc788c1fb04bc199ddb6
Artem F. <artfa63@gmail.com>
2024-10-13 23:59:57 +0400

    chore: Remove opening profile on login

--- app/src/main/java/com/example/yodreik/MainActivity.java
+++ app/src/main/java/com/example/yodreik/MainActivity.java
@@ -21,10 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        if (Preference.HasAccessToken(getApplicationContext())) {
-            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
-        }
     }
 
     public void loginButtonOnClick(View view) {
